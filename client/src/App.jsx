441f8eb2751dd5d72e78170437e1ee26b60387e2
rohan samanta <rohansamanta45@gmail.com>
2025-07-26 20:38:30 +0530

    Initialize server project with package.json and essential dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import {nanoid} from 'nanoid';
+import "./App.css";
+
+const socket = io("http://localhost:3000");
+const userId = nanoid(6); // Generate a unique ID for the user
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message.trim() === "") return;
+    socket.emit("chat", { message, userId });
+    setMessage("");
+  };
+
+  useEffect(() => {
+    socket.on("chat", (payload) => {
+      setChatHistory((prev) => [...prev, payload]);
+      console.log(chatHistory);
+    });
+
+    // Cleanup function to remove the event listener
+    return () => {
+      socket.off("chat");
+    };
+  }, []); // Empty dependency array means this effect runs only once
+
+  return (
+    <>
+      <div>
+        <h1>Chat Application</h1>
+        {chatHistory.map((chat, index) => (
+          <h2 key={index} className='chat-message'>
+            {chat.userId}: {chat.message}
+          </h2>
+        ))}
+        <form onSubmit={sendMessage}>
+          <input
+            type='text'
+            placeholder='Type your message here...'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type='submit'>Send</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
